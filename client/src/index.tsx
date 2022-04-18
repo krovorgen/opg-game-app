d047b3a5449dce05f5dfdab0e4b1a627fdd0872e
maksim <krovorgen@gmail.com>
2022-04-18 23:09:28 +0300

    updated for react18

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './scss/index.scss';
+import { Provider } from 'react-redux';
+
+import { createRoot } from 'react-dom/client';
+
 import App from './App';
 import { store } from './app/store';
-import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import './scss/index.scss';
 
-ReactDOM.render(
+createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change

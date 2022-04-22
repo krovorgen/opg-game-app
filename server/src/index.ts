1cf1eebbefced8172795ee18a001b55db1877447
maksim <krovorgen@gmail.com>
2022-04-22 12:44:06 +0300

    add .gitignore and config backend

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 import { usersRouter } from './routes/users-router';
 import { runDb } from './repositories/db';
@@ -11,8 +11,12 @@ const port = process.env.PORT || 4000;
 
 app.use('/api/users', usersRouter);
 
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello world');
+});
+
 const startApp = async () => {
-  await runDb();
+  // await runDb();
 
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);

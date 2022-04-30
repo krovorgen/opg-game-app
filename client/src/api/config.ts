54f4d04036cf890bb1fc9b4cce7b6e3f8fcef1fc
maksim <krovorgen@gmail.com>
2022-04-30 22:31:22 +0300

    fix for VPS Docker

--- client/src/api/config.ts
+++ client/src/api/config.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_API_URI;
+const BASE_URL = process.env.REACT_APP_API_URI || 'http://localhost:4000/api/';
 
 export const instance = axios.create({
   baseURL: BASE_URL,

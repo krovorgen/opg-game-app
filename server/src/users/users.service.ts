08f0c5b2c8c8677177a7428fd93702d53003375a
maksim <krovorgen@gmail.com>
2022-05-22 10:27:01 +0300

    get by id user

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,13 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { User, UserDocument } from '../schemas/users.schema';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 
+const privateFields = {
+  passwordHash: 0,
+  __v: 0,
+  emailConfig: 0,
+  _id: 0,
+};
+
 @Injectable()
 export class UsersService {
-  constructor(@InjectModel(User.name) private catModel: Model<UserDocument>) {}
+  constructor(
+    @InjectModel(User.name) private userRepository: Model<UserDocument>,
+  ) {}
 
   getUsers() {
-    return this.catModel.find().exec();
+    return this.userRepository.find({}, privateFields).exec();
+  }
+
+  async getById(id: number) {
+    const user = await this.userRepository.findOne({ id }).exec();
+    if (!user) throw new NotFoundException(`User with id ${id} not found`);
+    return user;
   }
 }
